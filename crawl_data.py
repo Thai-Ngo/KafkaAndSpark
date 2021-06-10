67eb26f5653f8fe946eae42fc65a8579b59ac8ce
Thai <ngominhhthai@gmail.com>
2021-06-10 20:53:22 +0700

    Crawl multiple stock

--- crawl_data.py
+++ crawl_data.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from datetime import datetime, timedelta
 import pandas as pd
-from kafka import KafkaProducer
+from selenium.webdriver.remote import webelement
 
 driver_location = "/usr/bin/chromedriver"
 binary_location = "/usr/bin/google-chrome"
@@ -15,39 +15,57 @@ options.binary_location = binary_location
 options.headless = True
 options.add_argument("--window-size=1920,1080")
 
-driver = webdriver.Chrome(executable_path=driver_location, options=options)
-driver.get("https://s.cafef.vn/Lich-su-giao-dich-VIC-1.chn")
+f = open("config.txt", 'r')
+num_stock = int(f.readline())
 
-date = datetime(year=2020, month=1, day=3)
-days_added = timedelta(days=1)
+driver = []
+start_date_box = []
+end_date_box = []
+search_button = []
+raw_data = [None] * num_stock
+stock_name = []
 
-start_date_box = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_dpkTradeDate1_txtDatePicker")
-end_date_box = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_dpkTradeDate2_txtDatePicker")
-search_button = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_btSearch")
+for i in range (num_stock):
+    driver.append(webdriver.Chrome(executable_path=driver_location, options=options))
+    stock_name.append(str(f.read(3)))
+    f.read(1)
+    
+    url = "https://s.cafef.vn/Lich-su-giao-dich-" + stock_name[i] + "-1.chn"
+    driver[i].get(url)
 
-time_now = datetime.now()
-raw_data = ""
+    date = datetime(year=2020, month=1, day=3)
+    days_added = timedelta(days=1)
+
+    start_date_box.append(driver[i].find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_dpkTradeDate1_txtDatePicker"))
+    end_date_box.append(driver[i].find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_dpkTradeDate2_txtDatePicker"))
+    search_button.append(driver[i].find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_btSearch"))
+
+    time_now = datetime.now()
+
 while date < time_now :
-    start_date_str = str(date.day) + '/' + str(date.month) + '/' + str(date.year)
-    end_date_str = str(date.day) + '/' + str(date.month) + '/' + str(date.year)
+    for i in range (num_stock):
+        start_date_str = str(date.day) + '/' + str(date.month) + '/' + str(date.year)
+        end_date_str = str(date.day) + '/' + str(date.month) + '/' + str(date.year)
 
-    start_date_box.clear()
-    start_date_box.send_keys(start_date_str)
+        start_date_box[i].clear()
+        start_date_box[i].send_keys(start_date_str)
     
-    end_date_box.clear()
-    end_date_box.send_keys(end_date_str)
-    search_button.click()
+        end_date_box[i].clear()
+        end_date_box[i].send_keys(end_date_str)
     
-    time.sleep(2)
-    try: 
-        raw_data = driver.find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_rptData2_ctl01_itemTR").text.split(sep=" ")
-        print ("VIC", date.date(), raw_data[21], raw_data[23], raw_data[19], raw_data[3], raw_data[1])
-    except NoSuchElementException:
-        print ("VIC", date.date(), raw_data[21], raw_data[23], raw_data[19], raw_data[3], raw_data[1])
+        search_button[i].click()
+    
+        try: 
+            raw_data[i] = driver[i].find_element_by_id("ctl00_ContentPlaceHolder1_ctl03_rptData2_ctl01_itemTR").text.split(sep=" ")
+            print (stock_name[i], date.date(), raw_data[i][21], raw_data[i][23], raw_data[i][19], raw_data[i][3], raw_data[i][1])
+        except NoSuchElementException:
+            print (stock_name[i], date.date(), raw_data[i][21], raw_data[i][23], raw_data[i][19], raw_data[i][3], raw_data[i][1])
 
     date += days_added
+    time.sleep(2)
     
     
-
+for i in range (num_stock):
+    driver[i].close()
 
 
